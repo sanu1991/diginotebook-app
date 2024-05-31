9621618cff0adac68dfdd1fa3fb437d0423ae18c
Sayan <spandab.ee@gmail.com>
2024-05-31 16:03:43 +0530

    1changes310524

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import Container from './components/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
